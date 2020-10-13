53a2f20fe1182dd2ca780cdf61a3b891c76b835e
Jeremy Stein <saplyng@gmail.com>
2020-10-13 04:27:06 -0500

    Initial Commit

---
+++ swiftui navigation/swiftui_navigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_navigationApp.swift
+//  swiftui navigation
+//
+//  Created by student on 10/13/20.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_navigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
